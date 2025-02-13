c3fa8a764c846847d859d48ac36e0386a6a9418f
Benedikt Ursprung <bursprung@lbl.gov>
2025-02-13 13:44:10 +0100

    added licence, readme, quick.ui, mini.ui and made target positions protected.

---
+++ README.md
@@ -0,0 +1,34 @@
+ScopeFoundryHW.dynamixel_servo
+===========================
+
+ScopeFoundry hardware plug-in to control robotis dynamixel servo
+
+ScopeFoundry is a Python platform for controlling custom laboratory 
+experiments and visualizing scientific data
+
+<http://www.scopefoundry.org>
+
+This software is not made by or endorsed by the device manufacturer
+
+
+Author
+----------
+
+Edward Barnard
+Benedikt Ursprung
+
+Requirements
+------------
+
+	* ScopeFoundry
+	* dynamixel_sdk
+
+Install Dynamixel Wizard>2.0 from robotis website to look up and change the IDs of your servo motors. 
+
+	
+History
+--------
+
+### 0.1.0	2025-02-13	Initial public release.
+
+Plug-in has been used internally and has been stable.
--- servo_test_app.py
+++ servo_test_app.py
@@ -1,29 +1,52 @@
 from ScopeFoundry.base_app import BaseMicroscopeApp
-from ScopeFoundryHW.dynamixel_servo.dynamixel_x_servo_hw import DynamixelXServosHW
-from ScopeFoundryHW.dynamixel_servo.dynamixel_filter_wheel_hw import DynamixelFilterWheelHW
 
 
 class ServoTestApp(BaseMicroscopeApp):
-    
-    name = 'servo_test_app'
-    
+
+    name = "servo_test_app"
+
     def setup(self):
-        
-        servos = self.add_hardware(DynamixelXServosHW(self, devices=dict(reflector_wheel=33,)))
 
-        self.add_hardware(DynamixelFilterWheelHW(self, name='reflector_wheel',
-                                        named_positions={'empty':  (90, "lightgreen"),
-                                                         'mirror': (180, 'yellowgreen'),
-                                                         'glass':  (270, "lime"),
-                                                         },))
-                                                                                  
-        servos.settings['port'] = 'COM11'
-        servos.settings['connected'] = True
+        from ScopeFoundryHW.robotis_dynamixel_servo import DynamixelXServosHW
+        from ScopeFoundryHW.robotis_dynamixel_servo import DynamixelFilterWheelHW
+        from ScopeFoundryHW.robotis_dynamixel_servo import DynamixelServoHW
 
-        
-if __name__ == '__main__':
+        servos = self.add_hardware(
+            DynamixelXServosHW(self, devices=dict(reflector_wheel=51, test_wheel=51))
+        )
+
+        self.add_hardware(
+            DynamixelFilterWheelHW(
+                self,
+                name="reflector_wheel",
+                named_positions={
+                    "empty": (90, "lightgreen"),
+                    "mirror": (180, "yellowgreen"),
+                    "glass": (270, "lime"),
+                },
+            )
+        )
+
+        self.add_hardware(DynamixelServoHW(self, name="test_wheel"))
+
+        servos.settings["port"] = "COM10"
+        servos.settings["connected"] = True
+
+    def setup_ui(self):
+        from qtpy import QtWidgets
+
+        widget = QtWidgets.QWidget()
+        widget.setMaximumWidth(380)
+        layout = QtWidgets.QVBoxLayout(widget)
+        self.add_quickbar(widget)
+        layout.addWidget(self.hardware["test_wheel"].New_mini_UI())
+        layout.addWidget(self.hardware["test_wheel"].New_quick_UI())
+
+
+if __name__ == "__main__":
 
     import sys
+
     app = ServoTestApp(sys.argv)
     # app.qtapp.setStyle('Fusion')
     sys.exit(app.exec_())
